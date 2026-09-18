7ceb9d0cad4ed183c833f902e29ceb7c467651eb
Rinkesh375 <rinkeshujjwal16@gmail.com>
2026-09-18 21:26:20 +0530

    hash,hgetall,hset,hdel in redis

--- user-profile-cached-hashed-json/src/index.js
+++ user-profile-cached-hashed-json/src/index.js
@@ -2,30 +2,37 @@ import express from "express";
 import Redis from "ioredis";
 import mongoose, { mongo } from "mongoose";
 
+const app = express();
+app.use(express.json())
 
-const app = express()
+export const redis = new Redis(
+  process.env.REDIS_URL || "redis://localhost:6379",
+);
 
+app.post("/user/:id/json", async (req, res) => {
+  const body = req.body
+  await redis.set(`user:${req.params.id}:json`, JSON.stringify( body ));
+  res.json({ success: true, storeAs: "JSON" });
+});
 
-export const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379")
-
-
-app.get("/redis",async(req,res)=>{
-    const reply = await redis.ping();
-    res.json({redis:reply})
-})
-
-
-app.get("/mongo",async(req,res)=>{
-    const url = process.env.MONGO_URL ||  "mongodb://localhost:27017/mongodb"
-
-    if(mongoose.connection.readyState === 0){
-        await mongoose.connect(url)
-    }
-
-    res.json({mongo:"connected",database:mongoose.connection.name})
-})
+app.get("/user/:id/json", async (req, res) => {
+  const data = await redis.get(`user:${req.params.id}:json`);
+  console.log(data)
+  res.json({ success: true, data:JSON.parse(data) });
+});
 
 
-app.listen(3000,()=>{
-    console.log("Application runing at port:3000")
-})
+app.post("/user/:id/hash", async (req, res) => {
+  const body = req?.body;
+  await redis.hset(`user:${req.params.id}:hash`, body);
+  res.json({ success: true, storeAs: "hash" });
+});
+
+app.get("/user/:id/hash", async (req, res) => {
+  const data = await redis.hgetall(`user:${req.params.id}:hash`);
+  res.json({ success: true, data });
+});
+
+app.listen(3000, () => {
+  console.log("Application runing at port:3000");
+});
