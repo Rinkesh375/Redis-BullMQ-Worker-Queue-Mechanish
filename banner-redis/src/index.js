a5b35460b0981a6711804391b6c284ac99ba73e3
Rinkesh375 <rinkeshujjwal16@gmail.com>
2026-09-18 10:00:49 +0530

    added redis set,get,delete,exists

---
+++ banner-redis/src/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import Redis from "ioredis";
+
+
+
+const app = express()
+app.use(express.json())
+
+
+export const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379")
+
+const bannerKey = "banner:app"
+
+app.get("/redis",async(req,res)=>{
+    const reply = await redis.ping();
+    res.json({redis:reply})
+})
+
+
+app.post("/banner",async(req,res)=>{
+    await redis.set(bannerKey,req?.body?.message ?? "Hi User welcome")
+    res.json({result:"body message set successfully"})
+})
+
+app.get("/banner",async(req,res)=>{
+  const response =  await redis.get(bannerKey)
+  res.json({result:response})
+})
+
+
+app.get("/banner/exists",async(req,res)=>{
+  const response =  await redis.exists(bannerKey)
+  res.json({result:response})
+})
+
+
+app.delete("/banner",async(req,res)=>{
+  const response =  await redis.del(bannerKey)
+  res.json({result:"Banner Key deleted successfully"})
+})
+
+
+app.listen(3000,()=>{
+    console.log("Application runing at port:3000")
+})
